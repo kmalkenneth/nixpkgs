dac0657820d26b6436e639cd6e805b77072cfdf9
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-24 23:58:54 +0100

    fixup! python3Packages.pysdl2: 0.9.16 -> 0.9.17

--- pkgs/development/python-modules/pysdl2/default.nix
+++ pkgs/development/python-modules/pysdl2/default.nix
@@ -4,6 +4,7 @@
   replaceVars,
   fetchPypi,
   buildPythonPackage,
+  setuptools,
   SDL2,
   SDL2_ttf,
   SDL2_image,
@@ -14,29 +15,15 @@
 buildPythonPackage rec {
   pname = "pysdl2";
   version = "0.9.17";
+  pyproject = true;
 
-  # The tests use OpenGL using find_library, which would have to be
-  # patched; also they seem to actually open X windows and test stuff
-  # like "screensaver disabling", which would have to be cleverly
-  # sandboxed. Disable for now.
-  doCheck = false;
   pythonImportsCheck = [ "sdl2" ];
 
   src = fetchPypi {
-    pname = "PySDL2";
-    inherit version;
+    inherit pname version;
     hash = "sha256-SMbvAaTrEj219+RuGhtWVnV1WwfmFfP+IKYjyUc1tSs=";
   };
 
-  # Deliberately not in propagated build inputs; users can decide
-  # which library they want to include.
-  buildInputs = [
-    SDL2_ttf
-    SDL2_image
-    SDL2_gfx
-    SDL2_mixer
-  ];
-  propagatedBuildInputs = [ SDL2 ];
   patches = [
     (replaceVars ./PySDL2-dll.patch (
       builtins.mapAttrs
@@ -52,6 +39,25 @@ buildPythonPackage rec {
         }
     ))
   ];
+
+  build-system = [ setuptools ];
+
+  # Deliberately not in propagated build inputs; users can decide
+  # which library they want to include.
+  buildInputs = [
+    SDL2_ttf
+    SDL2_image
+    SDL2_gfx
+    SDL2_mixer
+  ];
+
+  dependencies = [ SDL2 ];
+
+  # The tests use OpenGL using find_library, which would have to be
+  # patched; also they seem to actually open X windows and test stuff
+  # like "screensaver disabling", which would have to be cleverly
+  # sandboxed. Disable for now.
+  doCheck = false;
 
   meta = {
     description = "Wrapper around the SDL2 library and as such similar to the discontinued PySDL project";
