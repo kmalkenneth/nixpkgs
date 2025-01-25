1df957831cc7a982d3af0cdba7866288c4cac676
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-25 21:45:56 +0100

    python312Packages.backports-shutil-which: init at 3.5.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1474,6 +1474,8 @@ self: super: with self; {
 
   backports-entry-points-selectable = callPackage ../development/python-modules/backports-entry-points-selectable { };
 
+  backports-shutil-which = callPackage ../development/python-modules/backports-shutil-which { };
+
   backports-strenum = callPackage ../development/python-modules/backports-strenum { };
 
   backports-tarfile = callPackage ../development/python-modules/backports-tarfile { };
