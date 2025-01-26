04417f6e43997252f7f1c9a739b1ecbab108c4d7
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-26 02:43:11 +0100

    fixup! python3Packages.durus: 4.2 -> 4.3

--- pkgs/development/python-modules/durus/default.nix
+++ pkgs/development/python-modules/durus/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "durus";
   version = "4.3";
+  pyproject = true;
 
   src = fetchPypi {
-    pname = "Durus";
-    inherit version;
+    inherit pname version;
     hash = "sha256-aQM0I26juo2WbjrszgJUd5CdayQNCzID0zJ/YkNyYAc=";
   };
+
+  build-system = [ setuptools ];
 
   # Checks disabled due to missing python unittest framework 'sancho' in nixpkgs
   doCheck = false;
