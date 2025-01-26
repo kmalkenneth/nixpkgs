e0b594b122fe02d4373d982588822e3a71e388f8
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-26 03:37:52 +0100

    fixup! python3Packages.python-ly: 0.9.7 -> 0.9.9

--- pkgs/development/python-modules/python-ly/default.nix
+++ pkgs/development/python-modules/python-ly/default.nix
@@ -1,25 +1,38 @@
 {
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   lib,
+  hatchling,
+  lxml,
+  pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "python-ly";
   version = "0.9.9";
-  format = "setuptools";
+  pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-zxeA/lPTZ+/B8mQst3xXJGEG6nUX+MLREm8KNu4mVno=";
+  src = fetchFromGitHub {
+    owner = "frescobaldi";
+    repo = "python-ly";
+    tag = "v${version}";
+    hash = "sha256-CMMssU+qoHbhdny0sgpoYQas4ySPVHnu7GPnSthuMuE=";
   };
 
-  # tests not shipped on `pypi` and
-  # seem to be broken ATM: https://github.com/wbsoft/python-ly/issues/70
+  build-system = [ hatchling ];
+
+  # Tests seem to be broken ATM: https://github.com/wbsoft/python-ly/issues/70
   doCheck = false;
 
+  nativeCheckInputs = [
+    lxml
+    pytestCheckHook
+  ];
+
   meta = with lib; {
+    changelog = "https://github.com/frescobaldi/python-ly/releases/tag/${src.tag}";
     description = "Tool and library for manipulating LilyPond files";
+    homepage = "https://github.com/frescobaldi/python-ly";
     license = licenses.gpl2;
     maintainers = [ ];
   };
